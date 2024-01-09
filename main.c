9d5dc761c02ace176238c77a9b88fdacd5620150
sofieneb24 <150902000+sofieneb24@users.noreply.github.com>
2024-01-09 15:30:24 +0000

    :v.0.0.1 : add int float and morre

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main()
 {
+ int num =20;
+ int num2 =40;
+ float price=10.99;
  printf("Hello World\n");
+ printf("%d\n",num);
+ printf("%d\n",num2);
+ printf("la valeur de num = %d, la valeur de num2= %i\n, prix= %.2f\n", num, num2, price);
  return 0;
 }
